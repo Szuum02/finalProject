e9752f13abb9dd76e8783a4204230dbda9b4bdfa
Szuum02 <s.urbanski2002@gmail.com>
2023-09-21 13:40:40 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/visitsplanner/VisitsPlannerApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.visitsplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitsPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisitsPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/visitsplanner/VisitsPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.visitsplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitsPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
